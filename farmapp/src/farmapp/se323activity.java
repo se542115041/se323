e1e9ce431dd77c2ac0629e2963c5e7d48f01b20b
se542115041 <se542115041@vr.camt.info>
2017-02-10 10:07:46 +0700

    Adding a comment

--- farmapp/src/farmapp/se323activity.java
+++ farmapp/src/farmapp/se323activity.java
@@ -4,7 +4,7 @@ public class se323activity {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello!!");
+		System.out.println("hello!!"); // I changed the code
 	}
 
 }
