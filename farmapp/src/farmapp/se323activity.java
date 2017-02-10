9c30a6322e90f69bec5726072be3a1d2c757fdf8
se542115041 <se542115041@vr.camt.info>
2017-02-10 10:17:56 +0700

    yolo

--- farmapp/src/farmapp/se323activity.java
+++ farmapp/src/farmapp/se323activity.java
@@ -4,11 +4,8 @@ public class se323activity {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("hello!!"); // I changed the code
-=======
-		System.out.println("hello!!"); // i just change the code comment
->>>>>>> origin/master
+		System.out.println("oh man");
 	}
 
 }
