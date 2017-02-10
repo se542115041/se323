8dade402bf97c49bcf26f15f700f449819d06451
tanaphum <tana_jumbo@hotmail.com>
2017-02-10 10:59:31 +0700

    yolo

--- farmapp/src/farmapp/se323activity.java
+++ farmapp/src/farmapp/se323activity.java
@@ -4,11 +4,9 @@ public class se323activity {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("hello!!"); // I changed the code
-=======
-		System.out.println("hello!!"); // i just change the code comment
->>>>>>> origin/master
+		System.out.println("oh man");
+		System.out.println("ggwp");
 	}
 
 }
