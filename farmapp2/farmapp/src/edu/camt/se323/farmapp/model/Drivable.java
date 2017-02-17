cafafa7396ebdb5b8136821041c2e5e2ffba659e
se552115502 <909162722@qq.com>
2017-02-17 11:37:50 +0700

    Deleting farm app 2

--- farmapp2/farmapp/src/edu/camt/se323/farmapp/model/Drivable.java
+++
@@ -1,5 +0,0 @@
-package edu.camt.se323.farmapp.model;
-
-public interface Drivable {
-void drive();
-}
--- farmapp2/farmapp/src/edu/camt/se323/farmapp/model/Vechicle.java
+++
@@ -1,6 +0,0 @@
-package edu.camt.se323.farmapp.model;
-
-public interface Vechicle {
-	String name="";
-	int EnginePower = 0;
-}
