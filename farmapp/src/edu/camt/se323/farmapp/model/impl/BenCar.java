59e95005e67ac5afa42644548fc0755ef9f4a28a
se552115502 <909162722@qq.com>
2017-02-23 11:54:25 +0700

    Udating git ignore file, adding new class, commiting eclipse project files

---
+++ farmapp/src/edu/camt/se323/farmapp/model/impl/BenCar.java
@@ -0,0 +1,14 @@
+package edu.camt.se323.farmapp.model.impl;
+
+import edu.camt.se323.farmapp.model.Drivable;
+import edu.camt.se323.farmapp.model.Vechicle;
+
+public class BenCar implements Vechicle, Drivable {
+
+	@Override
+	public void drive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
